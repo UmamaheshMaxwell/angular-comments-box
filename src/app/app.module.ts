274d3f5d02dc041e30f6ea0d3995eccd444cf8cf
Umamaheswararao Meka <maheshmeka16@gmail.com>
2023-09-29 13:57:08 +0000

    added basic repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserCommentsComponent } from './user-comments/user-comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCommentsComponent
   ],
   imports: [
     BrowserModule,
